3b1638fe0b2e505bdf0abe2177661462a03cd508
Woody Peterson <woody@CCiMacDev.local>
2008-11-12 17:18:49 -0800

    fixed wordage in generated request specs

--- consumer_generators/request/templates/spec/request_spec.rb
+++ consumer_generators/request/templates/spec/request_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 describe <%= request_class %> do
   
-  it "should get rates with a canned response" do
+  it "should work with a canned response" do
     file = "spec/xml/<%= response_xml %>"
     xml = File.read("#{file}")
     raise "need to put example response in #{file}" if xml.blank?
@@ -12,27 +12,26 @@ describe <%= request_class %> do
     http.stub!(:use_ssl=)
     http.stub!(:verify_mode=)
     Net::HTTP.should_receive(:new).and_return(http)
-    
+  
     do_request
   end
 
   # run "DO_IT_LIVE=true spec spec" to contact the api
   if ENV['DO_IT_LIVE']
-    it "should get live rates" do
+    it "should work live" do
       $DEBUG = true # spit out xml for the request & response
       do_request
     end
   end
-  
+
   def do_request
     args = {
       # :attribute => "value"
     }
     raise "need to populate args for request" if args.empty?
-    
+  
     result = <%= request_class %>.new(args).do
 
     result.should_not be_blank
   end
-
 end
